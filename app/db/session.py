a09f11bb5ec7aca709c63d2de9b0e972c2dae0ff
Zuzanna Zajkiewicz <machefi1@gmail.com>
2022-01-12 21:27:50 +0100

    Working version

--- app/db/session.py
+++ app/db/session.py
@@ -8,7 +8,7 @@ PASSWORD = os.getenv("PASSWORD")
 DATABASE = os.getenv("DATABASE")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-
+# "driver://USER:PASSWORD@host:port"
 conn_str = f"mongodb://{USER}:{PASSWORD}@mongo:27017/"
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
 
--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
+import socket
+import select
+import errno
+import sys
+import os
+
 from fastapi import (
     FastAPI,
     Request,
@@ -12,8 +18,7 @@ from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
 from starlette.config import Config
 from starlette.requests import Request
 from starlette.middleware.sessions import SessionMiddleware
-from fastapi.openapi.docs import get_swagger_ui_html
-from fastapi.openapi.utils import get_openapi
+
 
 from authlib.integrations.starlette_client import OAuth
 
@@ -53,28 +58,6 @@ oauth.register(
 )
 
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
-
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     user = request.session.get("user")
@@ -108,21 +91,3 @@ async def logout(request: Request):
     request.session.pop("user", None)
 
     return RedirectResponse(url="/")
-
-
-@app.get("/chat", response_class=HTMLResponse)
-async def chat(request: Request):
-    return templates.TemplateResponse("chat.html", {"request": request})
-
-
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
